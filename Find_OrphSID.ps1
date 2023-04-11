7b42f6bb74b792fcaf89529fdc9c4d301a5e6eb6
HeeresS <48584256+HeeresS@users.noreply.github.com>
2023-04-11 15:33:23 +0200

    Update Find_OrphSID.ps1

--- Find_OrphSID.ps1
+++ Find_OrphSID.ps1
@@ -1,27 +1,29 @@
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true, HelpMessage="Specify the name of the Active Directory server")]
-    [string]$ServerName
-)
-Import-Module ActiveDirectory
-$orphans = @()
-$domain = Get-ADDomain -Server $ServerName
-$sidFilter = "SID=*"
-$searcher = New-Object System.DirectoryServices.DirectorySearcher
-$searcher.SearchRoot = [ADSI]"LDAP://$($domain.DistinguishedName)"
-$searcher.Filter = $sidFilter
-$searcher.PageSize = 1000
-$searcher.FindAll() | ForEach-Object {
-    $objectSID = New-Object System.Security.Principal.SecurityIdentifier($_.Properties['objectSID'][0],0)
-    $sidString = $objectSID.Value
-    $distinguishedName = $_.Properties['distinguishedName']
-    $class = $_.Properties['objectClass'][0]
-    if ($class -ne "computer" -and $class -ne "user" -and $class -ne "group") {
-        $orphans += @{
-            SID = $sidString;
-            DN = $distinguishedName;
-            Class = $class
+Function Get-OrphanedADObjects {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Specify the name of the Active Directory server")]
+        [string]$ServerName
+    )
+    Import-Module ActiveDirectory
+    $orphans = @()
+    $domain = Get-ADDomain -Server $ServerName
+    $sidFilter = "SID=*"
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher
+    $searcher.SearchRoot = [ADSI]"LDAP://$($domain.DistinguishedName)"
+    $searcher.Filter = $sidFilter
+    $searcher.PageSize = 1000
+    $searcher.FindAll() | ForEach-Object {
+        $objectSID = New-Object System.Security.Principal.SecurityIdentifier($_.Properties['objectSID'][0],0)
+        $sidString = $objectSID.Value
+        $distinguishedName = $_.Properties['distinguishedName']
+        $class = $_.Properties['objectClass'][0]
+        if ($class -ne "computer" -and $class -ne "user" -and $class -ne "group") {
+            $orphans += @{
+                SID = $sidString;
+                DN = $distinguishedName;
+                Class = $class
+            }
         }
     }
+    $orphans
 }
-$orphans
